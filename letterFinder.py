f76165095472b422245a1cec308ebac8b88993b9
Tyler Veeman <veemans3@gmail.com>
2018-10-04 17:08:04 -0700

    Image Segmentation updated

--- letterFinder.py
+++ letterFinder.py
@@ -4,22 +4,29 @@ import numpy
 from imutils.perspective import four_point_transform
 from imutils import contours
 import imutils
+import math
+from skimage.feature import hog
 
 
-img = cv2.imread("digits.jpg")
-img = cv2.resize(img, None, fx=1.5, fy=1.5)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
-edged = cv2.Canny(gray, 200, 200)
-img2, contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-avg_area = 0
-for c in contours:
-    avg_area = avg_area + cv2.contourArea(c)
-for c in contours:
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-    x, y, w, h = cv2.boundingRect(approx)
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
-cv2.imshow("Boxes", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def img_to_array(img_name):
+    img = cv2.imread(img_name)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 200, 200)
+
+    _, contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    rects = [cv2.boundingRect(c) for c in contours]
+    individual_images = []
+
+    for rect in rects:
+        x1, y1, x2, y2 = rect
+        cv2.rectangle(img, (x1, y1), (x1 + x2, y1 + y2), (0, 255, 0), 3)
+        roi = edged[y1:y1+y2, x1:x1+x2]
+        height, width = roi.shape
+        if height != 0 and width != 0:
+            roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+            roi = cv2.dilate(roi, (3, 3))
+
+            individual_images.append(roi)
+
+    return individual_images
