65bd08d7992a04165267d230b787af993cdd47d9
dr-p <christopher.dalporto@outlook.com>
2018-11-13 00:29:51 -0800

    updated model.fit and training code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import tensorflow as tf
 from tensorflow import keras
 import letterFinder
@@ -17,6 +18,7 @@ import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot
 from keras import backend as K
+from keras.datasets import mnist
 K.set_image_dim_ordering('th')
 
 useUI = False
@@ -24,30 +26,30 @@ useUI = False
 
 def main():
     my_file = Path("my_model.h5")
-    (train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
     train_images = train_images / 255.0
     test_images = test_images / 255.0
 
     #chris added
-    #train_images = train_images.reshape(train_images.shape[0], 1, 28, 28)
-    #test_images = test_images.reshape(test_images.shape[0], 1, 28, 28)
+    train_images = train_images.reshape(train_images.shape[0], 1, 28, 28)
+    test_images = test_images.reshape(test_images.shape[0], 1, 28, 28)
 
-    #train_images = train_images.astype('float32')
-    #test_images = test_images.astype('float32')
+    train_images = train_images.astype('float32')
+    test_images = test_images.astype('float32')
 
-    #model = ImageDataGenerator(rotation_range=90)
-    #model.fit(train_images)
-    #model = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
-    #model.fit(train_images)
-    #model = ImageDataGenerator(zca_whitening=True)
-    #model.fit(train_images)
-    #model = ImageDataGenerator(rotation_range=90)
-    #model.fit(train_images)
+    model = ImageDataGenerator(rotation_range=90)
+    model.fit(train_images)
+    model = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
+    model.fit(train_images)
+    model = ImageDataGenerator(zca_whitening=True)
+    model.fit(train_images)
+    model = ImageDataGenerator(rotation_range=90)
+    model.fit(train_images)
     #model = ImageDataGenerator(width_shift_range=shift, height_shift_range=shift)
     #model.fit(train_images)
-    #model = ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
-    #model.fit(train_images)
+    model = ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
+    model.fit(train_images)
 
     
     if my_file.exists():
@@ -65,7 +67,7 @@ def main():
                       loss='sparse_categorical_crossentropy',
                       metrics=['accuracy'])
         
-        model.fit(train_images, train_labels, epochs=5)
+        #model.fit(train_images, train_labels, epochs=5)
         model.save("my_model.h5")
 
 
